c8d8a72c311e0d323b76d4eee7ebc6d4b1f112fb
handrawanw <handrawanw@gmail.com>
2020-04-05 01:23:34 +0700

    Updates

---
+++ precache-manifest.7953545ee3c9b2ef4bed56dc43182e03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a6cf6dcd751a444c3738d672e1fc99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e93519bb58f69cefd653",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e93519bb58f69cefd653",
+    "url": "/static/js/2.57479fad.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/static/js/2.57479fad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff5d1c4fb70e4c772a63",
+    "url": "/static/js/main.67f9fe12.chunk.js"
+  },
+  {
+    "revision": "f07aeb5db8ebf956ad72",
+    "url": "/static/js/runtime-main.af348a9d.js"
+  }
+]);
